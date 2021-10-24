5c07afbd66071e2c89ee7e5c9dafc4c65b3365f7
hychan0517 <53807283+hychan0517@users.noreply.github.com>
2021-10-24 23:18:13 +0900

    플레이어카메라 상승 오토모드 추가

--- Assets/Scripts/InGame/Player/PlayerCamera.cs
+++ Assets/Scripts/InGame/Player/PlayerCamera.cs
@@ -45,12 +45,19 @@ public class PlayerCamera : MonoBehaviour
 		_deltaTime += Time.deltaTime;
 		float rate = _deltaTime * (1 / UPDATE_DURATION);
 
-		if (Mathf.Abs(transform.position.y - _player.transform.position.y) >= 4 && _player._isGround == false)
+		if (transform.position.y - _player.transform.position.y >= 6 && _player._isGround == false)
 		{
 			if (_isAuto == false)
 				Debug.Log("AUTO");
 			_isAuto = true;
-			transform.position = new Vector3(_startMovePosition.x + (_cameraMoveDirection.x * rate), _player.transform.position.y + 4, _startMovePosition.z + (_cameraMoveDirection.z * rate));
+			transform.position = new Vector3(_startMovePosition.x + (_cameraMoveDirection.x * rate), _player.transform.position.y + 6, _startMovePosition.z + (_cameraMoveDirection.z * rate));
+		}
+		else if (transform.position.y - _player.transform.position.y <= -5 && _player._isGround == false)
+		{
+			if (_isAuto == false)
+				Debug.Log("AUTO");
+			_isAuto = true;
+			transform.position = new Vector3(_startMovePosition.x + (_cameraMoveDirection.x * rate), _player.transform.position.y - 5, _startMovePosition.z + (_cameraMoveDirection.z * rate));
 		}
 		else
 		{
